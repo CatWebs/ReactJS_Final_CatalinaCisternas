e793e82f34f3d09b58abf81ebe4097d7f21a052e
[CatWebs] <catalina.cisternas@outlook.com>
2024-08-18 03:21:05 -0400

    vaciar carrito, eliminar producto y generar orden

--- src/componets/CartWidget/Cart.jsx
+++ src/componets/CartWidget/Cart.jsx
@@ -5,14 +5,12 @@ import { useState } from 'react'
 import "./Cart.css"
 
 const Cart = () => {
-  const { carrito/*, crearOrden,  quitarDelCarrito, vaciarCarrito*/ } = useAppContext();
+  const { carrito, crearOrden,  quitarDelCarrito, vaciarCarrito } = useAppContext();
     
-    /*const handleCarrito = () =>{
+    const handleCarrito = () =>{
       crearOrden();
-    }*/
-   console.log(carrito)
-
-   const [productoCantidad, setProductoCantidad] = useState(1)
+    }
+    const [productoCantidad, setProductoCantidad] = useState(1)
     const agregar = () => setProductoCantidad(productoCantidad + 1)
     const quitar = () => setProductoCantidad(productoCantidad - 1)
 
@@ -31,30 +29,24 @@ const Cart = () => {
         carrito.map((el)=> {
           return (
             <>
-              <div className='producto-carrito-header'>
-                <h2>Producto</h2>
-                <h2>Nombre</h2>
-                <h2>Cantidad</h2>
-                <h2>Precio</h2>
-              </div>
               <div className='producto-carrito' key={el.id}>
                 <img src={el.imagen}/>
                 <h4>{el.nombre}</h4>
                 <button className="boton-accion" onClick={quitar} disabled={productoCantidad < 2}>-</button>
                 <h3>{productoCantidad}</h3>
                 <button className="boton-accion" onClick={agregar} disabled={productoCantidad === 10 /*stock*/}>+</button>
-                <button className='boton-accion eliminar-producto-boton' /*onClick={()=> quitarDelCarrito()}*/>Eliminar Producto</button>
+                <button className='boton-accion eliminar-producto-boton' onClick={()=> quitarDelCarrito(el.id)}>Eliminar Producto</button>
                 <h4>${el.precio}</h4>
-              </div>
-              <h4 className="total-compra">Total de la compra : 0</h4>
-              <div className='cart-footer'>
-                <button className="fin-compra" /*onClick={()=> handleCarrito()}*/>Finalizar Compra</button>
-                <button className="fin-compra" /*onClick={()=> vaciarCarrito()}*/>Vaciar Carrito</button>
               </div>
             </>
           )
         })
-      }       
+      }   
+      <h4 className="total-compra">Total de la compra : 0</h4>
+      <div className='cart-footer'>
+        <button className="fin-compra" onClick={()=> handleCarrito()}>Finalizar Compra</button>
+        <button className="fin-compra" onClick={()=> vaciarCarrito()}>Vaciar Carrito</button>
+      </div>    
     </div>
   )
 }
--- src/componets/Context/Context.jsx
+++ src/componets/Context/Context.jsx
@@ -1,12 +1,12 @@
 import { createContext, useContext, useState} from "react";
-import fetchData from "../../fetchData";
-//import Swal from "sweetalert2";
+//import fetchData from "../../fetchData";
+import Swal from "sweetalert2";
 
-{/*import { initializeApp } from "firebase/app";*/}
-{/*import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";*/}
+import { initializeApp } from "firebase/app";
+import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
 
 // Uso de Firebase
-/*const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAM3Xfb9GWV1oA6Mbb72gCLpKU4bgiTZp0",
     authDomain: "productos-cat-cisternas.firebaseapp.com",
     projectId: "productos-cat-cisternas",
@@ -19,18 +19,137 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const productsCollection = collection(db, "productos");
-const ordersCollection = collection(db, "ordenes")*/
+const ordersCollection = collection(db, "ordenes")
 
 
 // Uso de Context
 
+    const AppContext = createContext();
+
+    export const useAppContext = () => useContext(AppContext);
+
+    export const ContextProvider = (props) => {
+        const [productos, setProductos] = useState([]);
+        const [carrito, setCarrito] = useState([]);
 
 
-/*        getDocs(productsCollection).then(snapshot => {
-            let arrayProductos = snapshot.docs.map(el => el.data());
-            setProductos(arrayProductos);
-        }).catch(err => console.error(err));
-    }*/
+        function cargarData() {
+            /*fetchData()
+            .then(response => {
+            setProductos(response);
+            })
+            .catch(err => console.error(err));*/
+
+            getDocs(productsCollection).then(snapshot => {
+               let arrayProductos = snapshot.docs.map(el => el.data());
+                setProductos(arrayProductos);
+            }).catch(err => console.error(err));
+        };
+
+        function agregarAlCarrito(id) {
+            const carritoAuxiliar = [...carrito];
+            const productoAAgregar = productos.find(el => el.id === id);
+            carritoAuxiliar.push(productoAAgregar);
+            setCarrito(carritoAuxiliar);
+            Swal.fire({
+                title: "Éxito",
+                text: `Agregaste ${productoAAgregar.nombre} a tu carrito`,
+                icon: "success"
+            })
+        }
+    
+    // Crear Orden
+    function crearOrden(){
+       
+        const nuevaOrden = {
+            nombre: "Catalina Cisternas",
+            telefono: 98712532,
+            mail: "cata@coder.com",
+            productos: carrito,
+        };
+
+        addDoc(ordersCollection, nuevaOrden).then(response => {
+            Swal.fire({
+                title: "Listo",
+                text: `Tu orden se ha generado correctamente con el id ${response.id}`,
+                icon: "success"
+            });
+            setCarrito([]);
+        }).catch(err => {
+            alert("Algo salió mal, intente más tarde");
+            console.error(err);
+        });
+    }
+
+    // Quitar del carrito
+    function quitarDelCarrito(id) {
+        Swal.fire({
+            title: "Estás seguro?",
+            text: "Tu producto será eliminado",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Eliminar",
+            cancelButtonText: "Cancelar"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                let carritoAuxiliar = [...carrito].filter(el => el.id !== id);
+
+                setCarrito(carritoAuxiliar);
+
+                Swal.fire({
+                    title: "Eliminado",
+                    text: "Tu producto fue removido correctamente del carrito",
+                    icon: "success"
+                });
+            }
+        });
+    }
+
+    // Vaciar Carrito
+
+    function vaciarCarrito(){
+        Swal.fire({
+            title: "Estás seguro?",
+            text: "Todos los productos de tu carrito serán eliminados",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Eliminar",
+            cancelButtonText: "Cancelar"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                setCarrito([]);
+                Swal.fire({
+                    title: "Carrito Eliminado",
+                    text: "Se eliminaron todos los productos de tu carrito",
+                    icon: "success"
+                });
+            }
+        });
+    }
+
+ 
+    return(
+        <AppContext.Provider value={{productos,setProductos,carrito,agregarAlCarrito, cargarData, crearOrden, quitarDelCarrito, vaciarCarrito}}> 
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+
+
+
+
+
+
+//       getDocs(productsCollection).then(snapshot => {
+//            let arrayProductos = snapshot.docs.map(el => el.data());
+//            setProductos(arrayProductos);
+//        }).catch(err => console.error(err));
+    
 
     //Debe ser reemplazada por la de arriba
         /*function cargarData() {
@@ -51,145 +170,3 @@ const ordersCollection = collection(db, "ordenes")*/
         console.log(carrito)
     }*/
     
-
-    ////// NUEVO CODIGO A PROBAR
-    /*
-    
-
-    const carritoInicial = JSON.parse(localStorage.getItem("carrito")) || [];
-    const [carrito, setCarrito] = useState(carritoInicial);
-    
-        const agregarAlCarrito = (item, cantidad) => {
-            const itemAgregado = { ...item, cantidad };
-    
-            const nuevoCarrito = [...carrito];
-            const enCarrito = nuevoCarrito.find((el) => el.id === itemAgregado.id);
-    
-            if (enCarrito) {
-                enCarrito.cantidad += cantidad;
-            } else {
-                nuevoCarrito.push(itemAgregado);
-            }
-            setCarrito(nuevoCarrito);
-        };
-    
-        const cantidadCarrito = () => {
-            return carrito.reduce((acc, prod) => acc + prod.cantidad, 0);
-        };
-    
-        const precioFinal = () => {
-            return carrito.reduce((acc, prod) => acc + prod.precio * prod.cantidad, 0);
-        };
-    
-        const vaciarCarrito = () => {
-            setCarrito([]);
-        };
-    
-        useEffect(() => {
-            localStorage.setItem("carrito", JSON.stringify(carrito));
-        }, [carrito])*/
-    
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    /*
-    function crearOrden(){
-        if (carrito.length > 0) {
-            const nuevaOrden = {
-                nombre: "Catalina Cisternas",
-                telefono: 98712532,
-                mail: "cata@coder.com",
-                productos: carrito,
-            };
-
-            addDoc(ordersCollection, nuevaOrden).then(response => {
-                console.log("Orden creada correctamente con el id: " + response.id);
-                setCarrito([]);
-            }).catch(err => {
-                alert("Algo salió mal, intente más tarde");
-                console.error(err);
-            });
-        } else {
-            Swal.fire({
-                title: "",
-                text: "",
-                icon: "question"
-            });
-        }
-    }*/
-
-
-    const AppContext = createContext();
-
-    export const useAppContext = () => useContext(AppContext);
-
-    export const ContextProvider = (props) => {
-        const [productos, setProductos] = useState([]);
-        const [carrito, setCarrito] = useState([]);
-
-
-        function cargarData() {
-            fetchData()
-            .then(response => {
-            setProductos(response);
-            })
-            .catch(err => console.error(err));
-        };
-
-        function agregarAlCarrito(id) {
-            const carritoAuxiliar = [...carrito];
-            const productoAAgregar = productos.find(el => el.id === id);
-            carritoAuxiliar.push(productoAAgregar);
-            setCarrito(carritoAuxiliar);
-        }
-    
-    
- 
-    return(
-        <AppContext.Provider value={{productos,setProductos,carrito,agregarAlCarrito, cargarData}}> 
-            {props.children}
-        </AppContext.Provider>
-    )
-}
