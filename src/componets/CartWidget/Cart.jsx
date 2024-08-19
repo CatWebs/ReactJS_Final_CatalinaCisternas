8e011f3590a8ad3d1399291591331b513bda7e19
[CatWebs] <catalina.cisternas@outlook.com>
2024-08-19 00:59:40 -0400

    mejora en carrito

--- src/componets/CartWidget/Cart.jsx
+++ src/componets/CartWidget/Cart.jsx
@@ -16,40 +16,40 @@ const Cart = () => {
     const quitar = () => setProductoCantidad(productoCantidad - 1)
     */
 
-  return (
-    <div className='container-carrito'>
-      {
-        ((carrito.length) === 0) ?
-        <>
-          <h2 className="titulo-carrito-vacio">Tu carrito está vacío</h2>
-          <Link to={"/"}>
-            <button className="fin-compra">Volver</button>
-          </Link>
-        </>
-        :
-        carrito.map((el)=> {
-          return (
-            <>
-              <div className='producto-carrito' key={el.id}>
-                <img src={el.imagen}/>
-                <h4>{el.nombre}</h4>
-                <button className="boton-accion" /*onClick={quitar}*/ disabled={el.cantidad < 2}>-</button>
-                <h3>{el.cantidad}</h3>
-                <button className="boton-accion" /*onClick={agregar}*/ disabled={el.cantidad === el.stock}>+</button>
-                <button className='boton-accion eliminar-producto-boton' onClick={()=> quitarDelCarrito(el.id,el.precio,el.cantidad)}>Eliminar Producto</button>
-                <h4>${el.precio}</h4>
-              </div>
-            </>
-          )
-        })
-      }   
-      <h4 className="total-compra">Total de la compra : ${total}</h4>
-      <div className='cart-footer'>
-        <button className="fin-compra" onClick={()=> handleCarrito()}>Finalizar Compra</button>
-        <button className="fin-compra" onClick={()=> vaciarCarrito()}>Vaciar Carrito</button>
-      </div>    
-    </div>
-  )
-}
-
+    return (
+      <div className='container-carrito'>
+        {
+          ((carrito.length) === 0) ?
+          <>
+            <h2 className="titulo-carrito-vacio">Tu carrito está vacío</h2>
+            <Link to={"/"}>
+              <button className="fin-compra">Volver</button>
+            </Link>
+          </>
+          :
+          carrito.map((el)=> {
+            return (
+              <>
+                <div className='producto-carrito' key={el.id}>
+                  <img src={el.imagen}/>
+                  <h4>{el.nombre}</h4>
+                  <button className="boton-accion" /*onClick={quitar}*/ disabled={el.cantidad < 2}>-</button>
+                  <h3>{el.cantidad}</h3>
+                  <button className="boton-accion" /*onClick={agregar}*/ disabled={el.cantidad === el.stock}>+</button>
+                  <button className='boton-accion eliminar-producto-boton' onClick={()=> quitarDelCarrito(el.id,el.precio,el.cantidad)}>Eliminar Producto</button>
+                  <h4>${el.precio}</h4>
+                </div>
+              </>
+            )
+          })
+        }   
+        <h4 className="total-compra" hidden={carrito.length === 0}>Total de la compra : ${total}</h4>
+        <div className='cart-footer'>
+          <button className="fin-compra" onClick={()=> handleCarrito()} hidden={carrito.length === 0}>Finalizar Compra</button>
+          <button className="fin-compra" onClick={()=> vaciarCarrito()} hidden={carrito.length === 0}>Vaciar Carrito</button>
+        </div>    
+      </div>
+    )
+  }
+  
 export default Cart
