686a5ae752b1389543b621d30c25263bfe9e578f
[CatWebs] <catalina.cisternas@outlook.com>
2024-08-19 23:27:28 -0400

    Ahora si es el carrito

--- src/componets/CartWidget/Cart.jsx
+++ src/componets/CartWidget/Cart.jsx
@@ -1,33 +1,27 @@
 import React from 'react'
 import {Link }from 'react-router-dom'
 import { useAppContext } from "../Context/Context";
-//import { useState } from 'react'
+import { useState } from 'react'
 import "./Cart.css"
 
 const Cart = () => {
-  const { carrito, crearOrden,  quitarDelCarrito, vaciarCarrito, total } = useAppContext();
-    
-    const handleCarrito = () =>{
-      crearOrden();
-    }
-
-    return (
-      <div className='container-carrito'>
-        {
-          ((carrito.length) !== 0) ?
+  const { carrito,  quitarDelCarrito, vaciarCarrito, total } = useAppContext();
+  
+  return (
+    <div className='container-carrito'>
+      {
+        ((carrito.length) !== 0) ?
           carrito.map((el)=> {
             return (
-              <>
-                <div className='producto-carrito' key={el.id}>
-                  <img src={el.imagen}/>
-                  <h4>{el.nombre}</h4>
-                  {/*<button className="boton-accion" /*onClick={quitar}*/ /*disabled={el.cantidad < 2}>-</button>*/}
-                  <h3>{el.cantidad}</h3>
-                  {/*<button className="boton-accion" /*onClick={agregar}*/ /*disabled={el.cantidad === el.stock}>+</button>*/}
-                  <button className='boton-accion eliminar-producto-boton' onClick={()=> quitarDelCarrito(el.id,el.precio,el.cantidad)}>Eliminar Producto</button>
-                  <h4>${el.precio}</h4>
-                </div>
-              </>
+              <div className='producto-carrito' key={el.id}>
+                <img src={el.imagen}/>
+                <h4>{el.nombre}</h4>
+                {/*<button className="boton-accion" /*onClick={quitar}*/ /*disabled={el.cantidad < 2}>-</button>*/}
+                <h3>{el.cantidad}</h3>
+                {/*<button className="boton-accion" /*onClick={agregar}*/ /*disabled={el.cantidad === el.stock}>+</button>*/}
+                <button className='boton-accion eliminar-producto-boton' onClick={() => quitarDelCarrito(el.id, el.precio, el.cantidad)}>Eliminar Producto</button>
+                <h4>${el.precio}</h4>
+              </div>
             )
           })
           :
@@ -40,8 +34,12 @@ const Cart = () => {
         }   
         <h4 className="total-compra" hidden={carrito.length === 0}>Total de la compra : ${total}</h4>
         <div className='cart-footer'>
-          <button className="fin-compra" onClick={()=> handleCarrito()} hidden={carrito.length === 0}>Finalizar Compra</button>
-          <button className="fin-compra" onClick={()=> vaciarCarrito()} hidden={carrito.length === 0}>Vaciar Carrito</button>
+          <Link to="/checkout">
+            <button className="fin-compra" hidden={carrito.length === 0}>
+              Finalizar Compra
+            </button>
+          </Link>
+          <button className="fin-compra" onClick={() => vaciarCarrito()} hidden={carrito.length === 0}>Vaciar Carrito</button>
         </div>    
       </div>
     )
